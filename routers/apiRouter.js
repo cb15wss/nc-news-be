44c27cbf27173508c08484f9cdc2f87aad06f027
cb15wss <christinebanda1@gmail.com>
2020-02-06 11:31:58 +0000

    Pushed for checks

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -3,12 +3,15 @@ const topicsRouter = require("./topicsRouter");
 const usersRouter = require("./usersRouter");
 const articlesRouter = require("./articlesRouter");
 const commentsRouter = require("./commentsRouter");
-const sendApiJson = require("../controllers/apiController");
+const { jsonObj } = require("../endpoints.json");
+//const sendApiJson = require("../controllers/apiController");
 const { badMethod } = require("../errors/errors");
 
 apiRouter
   .route("/")
-  .get(sendApiJson)
+  .get(function(req, resp) {
+    resp.status(200).send({ jsonObj });
+  })
   .all(badMethod);
 
 apiRouter.use("/topics", topicsRouter);
