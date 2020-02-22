ea5d3a5dea93eb56ea6851127cbba5bc0dc4dd2a
cb15wss <christinebanda1@gmail.com>
2020-02-22 22:05:02 +0000

    Added cors script

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const { notRoute, customErrors, psqlErrors } = require("./errors/errors");
 const usersRouter = require("./routers/usersRouter");
 const articlesRouter = require("./routers/topicsRouter");
 const commentsRouter = require("./routers/commentsRouter");
+const cors = require("cors");
+
+api.use(cors());
 
 api.use(express.json());
 
